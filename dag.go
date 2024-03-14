d3c59fcbb7b030b0b15b2d11c75910a0b0031e64
tzy168 <3265577395@qq.com>
2024-03-15 00:20:36 +0800

    dag.go

--- dag.go
+++ dag.go
@@ -10,45 +10,70 @@ type Object struct {
 	Links []Link
 	Data  []byte
 }
-func Add(store KVStore, node Node, h hash.Hash) []byte {
-    switch node.Type() {
-    case FILE:
-        return storeFILE(store, node, h)
-    case DIR:
-        return storeDIR(store, node, h)
-    default:
-        // 未知的节点类型
-        return nil
-    }
-}
-
-func storeFILE(store KVStore, node Node, h hash.Hash) []byte {
-    data := node.(File).Bytes()
-    obj := Object{
-        Data: data,
-    }
-    hash := h.Sum(data)
-    store.Put(hash, obj)
-    return hash
-}
-
-func storeDIR(store KVStore, node Node, h hash.Hash) []byte {
-    dir := node.(Dir)
+func Add(store KVStore, node Node, h hash.Hash) ([]byte, error) {
     var links []Link
-    for it := dir.It(); it.Next(); {
-        hash := Add(store, it.Node(), h)
-        link := Link{
-            Name: it.Node().Name(),
-            Hash: hash,
-            Size: int(it.Node().Size()),
+
+    // 如果节点是一个目录，递归地添加其链接的节点
+    if dir, ok := node.(Dir); ok {
+        it := dir.It()
+        for it.Next() {
+            linkedNode := it.Node()
+            linkedHash, err := Add(store, linkedNode, h)
+            if err != nil {
+                return nil, err
+            }
+            // 创建一个Link并添加到links中
+            link := Link{
+                Name: linkedNode.Name(),
+                Hash: linkedHash,
+                Size: int(linkedNode.Size()),
+            }
+            links = append(links, link)
         }
-        links = append(links, link)
     }
-    obj := Object{
+
+    // 创建一个新的Object并将links添加到Object中
+    obj := &Object{
         Links: links,
+        Data:  data,
     }
-    h.Write(obj.Bytes())
-    hash := h.Sum(hash)
-    store.Put(hash, obj)
-    return hash
+
+    // 序列化Object的数据
+    objData, err := serializeObject(obj)
+    if err != nil {
+        return nil, err
+    }
+
+    // 计算Object的哈希值
+    h.Write(objData)
+    hashValue := h.Sum(nil)
+
+    // 将Object的数据存储在KVStore中
+    if err := store.Put(hashValue, objData); err != nil {
+        return nil, err
+    }
+
+    return hashValue, nil
 }
+func serializeNode(node Node) ([]byte, error) {  
+	func serializeNode(node Node) ([]byte, error) {
+        var data []byte
+    
+        // 将Size转换为字节切片
+        sizeBytes := make([]byte, 8)
+        binary.BigEndian.PutUint64(sizeBytes, node.Size())
+        data = append(data, sizeBytes...)
+    
+        // 将Name转换为字节切片
+        nameBytes := []byte(node.Name())
+        data = append(data, nameBytes...)
+    
+        // 将Type转换为字节切片
+        typeBytes := make([]byte, 4)
+        binary.BigEndian.PutUint32(typeBytes, uint32(node.Type()))
+        data = append(data, typeBytes...)
+    
+        return data, nil
+    }
+}  
+
