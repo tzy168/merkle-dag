42155721473c3e15adf6edd72e6af2a2756dafaf
tzy168 <3265577395@qq.com>
2024-03-29 10:20:08 +0800

    2

--- dag2file.go
+++ dag2file.go
@@ -1,7 +1,34 @@
 package merkledag
-
+import (
+	"encoding/json"
+	"strings"
+)
 // Hash to file
 func Hash2File(store KVStore, hash []byte, path string, hp HashPool) []byte {
 	// 根据hash和path， 返回对应的文件, hash对应的类型是tree
-	return nil
+		flag, _ := store.Has(hash)
+		if flag {
+			data, _ := store.Get(hash)
+			var obj Object
+			json.Unmarshal(data, &obj)
+			pathArr := strings.Split(path, "\\")
+			for _, link := range obj.Links {
+	
+				objType := string(data[len(data)-4:])
+				if link.Name == pathArr[0] {
+					if objType == "tree" {
+						return Hash2File(store, link.Hash, strings.Join(pathArr[1:], "\\"), hp)
+					} else {
+						continue
+					}
+				}
+				linkdata, err := store.Get(link.Hash)
+				if err != nil {
+					panic(err)
+				}
+				return linkdata
+			}
+	
+		}
+		return nil
 }
